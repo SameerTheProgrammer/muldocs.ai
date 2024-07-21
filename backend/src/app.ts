f3a050a8f6bc0cce1e6b1beae8a64122270d3286
Sameer kumar <2006Sameer2009@gmail.com>
2024-07-21 21:44:32 +0530

    add user register endopint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,10 @@
 import "reflect-metadata";
-import express, { NextFunction, Request, Response } from "express";
+import express, {
+    NextFunction,
+    Request,
+    RequestHandler,
+    Response,
+} from "express";
 import createHttpError, { HttpError } from "http-errors";
 import cors from "cors";
 import compression from "compression";
@@ -7,9 +12,11 @@ import rateLimit from "express-rate-limit";
 import cookieParser from "cookie-parser";
 import hpp from "hpp";
 import morgan from "morgan";
-import csrf from "csurf";
+// import csrf from "csurf";
 import sanitize from "express-mongo-sanitize";
 import logger from "./config/logger";
+import { AppDataSource } from "./config/data-source";
+import { User } from "./entity/User";
 
 // Initialize Express app
 const app = express();
@@ -20,15 +27,51 @@ app.use(express.json());
 app.use(cors());
 app.use(compression());
 app.use(cookieParser());
-app.use(csrf({ cookie: true }));
+// app.use(csrf({ cookie: true }));
 app.use(hpp());
 app.use(morgan("combined"));
 app.use(sanitize());
 app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Welcome to PdfAiChat site");
-});
+interface IUserRegisterRequest extends Request {
+    body: {
+        name: string;
+        email: string;
+        password: string;
+    };
+}
+
+app.post("/api/v1/auth/register", (async (
+    req: IUserRegisterRequest,
+    res: Response,
+    next: NextFunction,
+) => {
+    try {
+        const { name, email, password } = req.body;
+        logger.info("New request to register a user", {
+            name,
+            email,
+            password: "****",
+        });
+        const userRepository = AppDataSource.getRepository(User);
+        const isUserExist = await userRepository.findOne({ where: { email } });
+        if (isUserExist) {
+            const error = createHttpError(
+                400,
+                "Email is already exists. Try with different email",
+            );
+            return next(error);
+        }
+
+        const data = userRepository.create({ name, email, password });
+        const newUser = await userRepository.save(data);
+
+        logger.info("User has been registered", { id: newUser.id });
+        res.status(201).json({ newUser });
+    } catch (error) {
+        return next();
+    }
+}) as RequestHandler);
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises, @typescript-eslint/require-await
 app.get("/error", async (req: Request, res: Response, next: NextFunction) => {
