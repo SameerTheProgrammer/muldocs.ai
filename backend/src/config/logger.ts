8bd12eb51ee4d25cb54f4f61b43dfec86c92a471
Sameer kumar <2006Sameer2009@gmail.com>
2024-07-21 21:16:12 +0530

    testing to connect Database

--- backend/src/config/logger.ts
+++ backend/src/config/logger.ts
@@ -1,5 +1,5 @@
 import winston from "winston";
-import env from "./dotenv";
+// import env from "./dotenv";
 
 const logger = winston.createLogger({
     level: "info",
@@ -12,17 +12,17 @@ const logger = winston.createLogger({
             dirname: "logs",
             filename: "combined.log",
             level: "info",
-            silent: env.NODE_ENV == "test",
+            // silent: env.NODE_ENV == "test",
         }),
         new winston.transports.File({
             dirname: "logs",
             filename: "error.log",
             level: "error",
-            silent: env.NODE_ENV == "test",
+            // silent: env.NODE_ENV == "test",
         }),
         new winston.transports.Console({
             level: "info",
-            silent: env.NODE_ENV == "test",
+            // silent: env.NODE_ENV == "test",
         }),
     ],
 });
