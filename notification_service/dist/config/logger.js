2636632d91d1e9f9151fb089d680ed6a84c5f543
Sameer kumar <2006Sameer2009@gmail.com>
2024-08-04 22:17:40 +0530

    logger build file

--- notification_service/dist/config/logger.js
+++ notification_service/dist/config/logger.js
@@ -9,18 +9,6 @@ const logger = winston_1.default.createLogger({
     level: "info",
     format: winston_1.default.format.combine(winston_1.default.format.timestamp(), winston_1.default.format.json()),
     transports: [
-        new winston_1.default.transports.File({
-            dirname: "logs",
-            filename: "combined.log",
-            level: "info",
-            silent: dotenv_1.default.NODE_ENV == "test",
-        }),
-        new winston_1.default.transports.File({
-            dirname: "logs",
-            filename: "error.log",
-            level: "error",
-            silent: dotenv_1.default.NODE_ENV == "test",
-        }),
         new winston_1.default.transports.Console({
             level: "info",
             silent: dotenv_1.default.NODE_ENV == "test",
