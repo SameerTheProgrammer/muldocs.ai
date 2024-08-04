bc17eb3ef8c578b3417cb014d3b64952a3e866a4
Sameer kumar <2006Sameer2009@gmail.com>
2024-08-04 21:28:21 +0530

    editing dotenv configration for deployment

--- notification_service/src/config/dotenv.ts
+++ notification_service/src/config/dotenv.ts
@@ -2,13 +2,18 @@ import { config } from "dotenv";
 import { cleanEnv, port, str } from "envalid";
 import path from "path";
 
-config({
-    path: path.join(__dirname, `../../.env.${process.env.NODE_ENV || "dev"}`),
-});
+if (process.env.NODE_ENV !== "production") {
+    config({
+        path: path.join(
+            __dirname,
+            `../../.env.${process.env.NODE_ENV || "dev"}`,
+        ),
+    });
+}
 
 export const env = cleanEnv(process.env, {
     PORT: port(),
-    NODE_ENV: str({ default: "dev", choices: ["test", "prod", "dev"] }),
+    NODE_ENV: str({ default: "dev", choices: ["test", "production", "dev"] }),
 
     REDIS_SERVICE_URI: str(),
 
