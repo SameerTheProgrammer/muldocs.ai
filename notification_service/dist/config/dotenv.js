975c7cdd1fdbf72dd02ccfd150b54b6cacd2ddbe
Sameer kumar <2006Sameer2009@gmail.com>
2024-08-04 21:39:19 +0530

    adding dist folder

--- notification_service/dist/config/dotenv.js
+++ notification_service/dist/config/dotenv.js
@@ -7,12 +7,14 @@ exports.env = void 0;
 const dotenv_1 = require("dotenv");
 const envalid_1 = require("envalid");
 const path_1 = __importDefault(require("path"));
-(0, dotenv_1.config)({
-    path: path_1.default.join(__dirname, `../../.env.${process.env.NODE_ENV || "dev"}`),
-});
+if (process.env.NODE_ENV !== "production") {
+    (0, dotenv_1.config)({
+        path: path_1.default.join(__dirname, `../../.env.${process.env.NODE_ENV || "dev"}`),
+    });
+}
 exports.env = (0, envalid_1.cleanEnv)(process.env, {
     PORT: (0, envalid_1.port)(),
-    NODE_ENV: (0, envalid_1.str)({ default: "dev", choices: ["test", "prod", "dev"] }),
+    NODE_ENV: (0, envalid_1.str)({ default: "dev", choices: ["test", "production", "dev"] }),
     REDIS_SERVICE_URI: (0, envalid_1.str)(),
     EMAIL: (0, envalid_1.str)(),
     MAILING_ID: (0, envalid_1.str)(),
